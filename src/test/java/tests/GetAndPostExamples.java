2d530db71d86ee7636f2167f98f014b76ead5876
javlon91 <javlonbek91@gmail.com>
2024-09-09 09:10:03 +0500

    7 dasr

--- src/test/java/tests/GetAndPostExamples.java
+++ src/test/java/tests/GetAndPostExamples.java
@@ -1,7 +1,10 @@
 package tests;
 
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.testng.annotations.Test;
-
+import static org.hamcrest.Matchers.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,8 +28,26 @@ public class GetAndPostExamples {
 
     public void testPost(){
         Map<String,Object> map=new HashMap<String,Object>();
-        map.put("name","Raghav");
-        map.put("job","Teacher");
-        System.out.println(map);
+//        map.put("name","Raghav");
+//        map.put("job","Teacher");
+//        System.out.println(map);
+        JSONObject request = new JSONObject(map);
+        request.put("name","Raghav");
+        request.put("job","Teacher");
+
+        System.out.println(request);
+
+        baseURI = "https://reqres.in/api";
+
+        given().
+                header("Content-Type","application/json").
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                  body(request.toJSONString()).
+                when().
+                  post("/users").
+                then().
+                  statusCode(201)
+                .log().all();
     }
 }
